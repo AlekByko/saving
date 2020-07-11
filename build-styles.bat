3d40c7b7191e5cd052b3426697782434e7b59db0
AlekByko <>
2020-07-10 19:47:10 -0500

    calories working

--- build-styles.bat
+++ build-styles.bat
@@ -1,2 +1,3 @@
 node ./node_modules/less/bin/lessc ./styles/all.less ./styles/all.css
 node ./node_modules/less/bin/lessc ./styles/scanner.less ./styles/scanner.css
+node ./node_modules/less/bin/lessc ./styles/calories.less ./styles/calories.css
