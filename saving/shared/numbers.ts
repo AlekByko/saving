4b0fc26f8615721e6abc98c9427cdaad39d8e71f
AlekByko <>
2023-02-04 22:52:47 -0500

    click through relation

--- saving/shared/numbers.ts
+++ saving/shared/numbers.ts
@@ -1,3 +1,12 @@
+import { fail } from './core';
+
 export function safeRound(olderFavors: number, delta: number, magnitude: number) {
     return Math.round((olderFavors + delta) * magnitude) / magnitude;
+}
+
+export function nextAtAround<T>(values: T[], at: number, delta: number): number {
+    const { length } = values;
+    if (length < 1) return fail(nextAtAround.name + ': empty array');
+    if (length < delta) return fail(nextAtAround.name + ': delta bigger than length');
+    return (length + length + at + delta) % length;
 }
