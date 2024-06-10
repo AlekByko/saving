519a3586136334f6c13f736f01ec93d18b5417b7
AlekByko <>
2024-06-09 22:06:41 -0500

    saving tiles

--- config.py
+++ config.py
@@ -1,12 +1,7 @@
 import json
+from typing import List
 
 
-def load_config(path):
-    with open(path, 'r') as file:
-        data = json.load(file)
-        for config in data['caps']:
-            tile = Tile(config)
-            yield tile
 
 class Tile:
     def __init__(self, config):
@@ -18,3 +13,18 @@ class Tile:
         self.width = config['width']
         self.height = config['height']
         self.data = config
+
+def load_config(path):
+    with open(path, 'r') as file:
+        data = json.load(file)
+        for config in data['caps']:
+            tile = Tile(config)
+            yield tile
+
+def save_config(path, tiles: List[Tile]):
+    data = {
+        'caps': list(map(lambda x: x.data, tiles))
+    }
+    with open(path, "w") as file:
+        json.dump(data, file, indent=4)
+
