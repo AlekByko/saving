0eee882cc390c72aa1982e0207d88a8529ab7332
AlekByko <>
2024-06-18 18:56:06 -0500

    sort samples

--- train-from-samples.bat
+++ train-from-samples.bat
@@ -2,7 +2,7 @@ python start.py ^
     --mode train-from-samples ^
     --weights-path d:\___samples\320x240_weights_{epoch:02d}.h5 ^
     --samples-dir d:\___samples\320x240\ ^
-    --max-samples 13300 ^
+    --max-samples 23300 ^
     --epochs 120 ^
     --batch 4 ^
     --train-val-spit-at 0.90
