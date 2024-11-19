033e8ea14098c93b1220435e901b4b55d0432c46
AlekByko <>
2024-11-18 19:17:48 -0500

    progress dialog sandboxed

--- saving/client/progress-dialog.tsx
+++ saving/client/progress-dialog.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export interface ProgressDialogProps {
     progress: number;
@@ -13,4 +14,11 @@ export function thusProgressDialog() {
             </div>;
         }
     };
+}
+
+
+if (window.sandbox === 'progress-dialog') {
+    const ProgressDialog = thusProgressDialog();
+    const rootElement = document.getElementById('root')!;
+    ReactDOM.render(<ProgressDialog progress={0.734} />, rootElement);
 }
