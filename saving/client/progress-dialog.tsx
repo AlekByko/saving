f4c7f3f2e352f2a76aeee7f60d8ed80bca1a86d3
AlekByko <>
2024-11-25 20:31:20 -0500

    option to abort

--- saving/client/progress-dialog.tsx
+++ saving/client/progress-dialog.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import ReactDOM from 'react-dom';
+import { isUndefined } from './shared/core';
 
 export interface ProgressDialogProps {
     progress: number | string;
+    abort?: () => void
 }
 
 export function thusProgressDialog() {
     return class ProgressDialog extends React.Component<ProgressDialogProps> {
+        whenAborted: MouseEventHandler<HTMLButtonElement> = _e => {
+            const { abort } = this.props;
+            if (isUndefined(abort)) return;
+            abort();
+        };
         render() {
-            const { progress } = this.props;
+            const { progress, abort } = this.props;
             const text = typeof progress === 'string' ? progress : (progress * 100).toFixed(1);
-            return <div className="progress-dialog">{text}</div>;
+            return <React.Fragment>
+                <div className="progress-dialog">{text}</div>
+                {abort ?? <button className="off" onClick={this.whenAborted}>Abort</button>}
+            </React.Fragment>;
         }
     };
 }
