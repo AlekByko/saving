d04c268d018584ece12f7f31e37b79e2bd408122
AlekByko <>
2025-09-14 16:04:34 -0500

    renaming

--- saving/client/file-system-entries.ts
+++ saving/client/file-system-entries.ts
@@ -5,12 +5,12 @@ export interface KnownPickedDirEntry {
     handle: FileSystemDirectoryHandle;
 }
 
-export const knownBaseDirRef = 'base-dir' as KnownPickedDirRef;
+export const knownConfigsDirRef = 'base-dir' as KnownPickedDirRef;
 export const knownSnapsDirRef = 'recorded-dir' as KnownPickedDirRef;
 export const knownCapsDirRef = 'caps-dir' as KnownPickedDirRef;
 export const knownTrainingSetDirRef = 'known-training-set-dir' as KnownPickedDirRef;
 export const knownOutputDirRef = 'output-dir' as KnownPickedDirRef;
-export const knownConfigDirRef = 'config-dir' as KnownPickedDirRef;
+export const knownEdConfigDirRef = 'config-dir' as KnownPickedDirRef;
 export const knownArchiveDirRef = 'archive-dir' as KnownPickedDirRef;
 export const knownMatesDirRef = 'pairs-dir' as KnownPickedDirRef;
 export const knownFacesDirRef = 'faces-dir' as KnownPickedDirRef;
